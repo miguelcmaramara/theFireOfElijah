aef480b056cc403313743120f7b171af0b825207
miguelcmaramara <miguelcmaramara@gmail.com>
2020-12-23 12:30:25 -0500

    Platformio project creation

---
+++ TheFireOfElijah/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
