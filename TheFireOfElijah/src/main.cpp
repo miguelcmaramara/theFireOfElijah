6c7fe8ca27a124a614cf78bf4a5c6ae0dc9c84c3
miguelcmaramara <miguelcmaramara@gmail.com>
2021-01-08 18:39:02 -0500

    First prediction algorithm using skips to better fit to curve

--- TheFireOfElijah/src/main.cpp
+++ TheFireOfElijah/src/main.cpp
@@ -8,17 +8,18 @@ Pixy2 pixy;
  * Editable variables
  **/
 unsigned const int Y_POLY_DEGREE = 2;
-unsigned const int XNZ_POLY_DEGREE = 1; 
+unsigned const int XNZ_POLY_DEGREE = 2; 
 unsigned const int PROJ_SIGNATURE = 2; 
+unsigned const int SKIP_PARAM = 3; 
 
 // time variables
-// double, used to input to equation
 double t;
 unsigned long t0;
 unsigned long deltaT;
 
 // tracking variables;
 unsigned int count = 0;
+unsigned int trueCount = 0;
 int i; 
 int j; 
 int k;
@@ -29,6 +30,9 @@ double **z;
 double xPara[XNZ_POLY_DEGREE+1];
 double yPara[Y_POLY_DEGREE+1];
 double zPara[XNZ_POLY_DEGREE+1];
+int X[100];
+int Y[100];
+double T[100];
 
 void createXYZArr(){
   // Only purpose is to create arrays
@@ -45,7 +49,15 @@ void createXYZArr(){
     z[i] = new double [XNZ_POLY_DEGREE + 2];
 }
 
-
+//Logistical functions
+void printArr(unsigned long arr[],int size){
+  Serial.print("[");
+  for(int i = 0; i < size; i++){
+    Serial.print(arr[i]);
+    if(i < size - 1) Serial.print(", ");
+  }
+  Serial.print("]\n");
+}
 void printArr(double arr[],int size){
   Serial.print("[");
   for(int i = 0; i < size; i++){
@@ -54,15 +66,34 @@ void printArr(double arr[],int size){
   }
   Serial.print("]\n");
 }
-
-void timeArray(double arr[], unsigned int size, unsigned long time, int coord){
-  // this function fills all but the last of the array with time time
-  arr[0] = 1;
-  for(int i = 1; i < size - 1; i++)
-    arr[i] = (double)time/1000;
-  arr[size] = (double)coord;
+void printArr(int arr[],int size){
+  Serial.print("[");
+  for(int i = 0; i < size; i++){
+    Serial.print(arr[i]);
+    if(i < size - 1) Serial.print(", ");
+  }
+  Serial.print("]\n");
+}
+void fillTestValues(){
+  // fill values
+  k = 0;
+  for (i = 0; i < Y_POLY_DEGREE + 1; i++){
+    for (j = 0; j < Y_POLY_DEGREE + 2; j++){
+      y[i][j] = ++k;
+      Serial.print(y[i][j]);
+      Serial.print(" ");
+    }
+    printArr(y[i],Y_POLY_DEGREE + 2);
+  }
 }
 
+void timeArray(double arr[], unsigned int size, double time, int coord){
+  // this function fills all but the last of the array with time time
+  arr[0] = 1;
+  for(int i = 1; i < size; i++)
+    arr[i] = (double)time;
+  arr[size - 1] = (double)coord;
+}
 void curveFit(double **arr, double res[], unsigned int resSize){
   int i;
   int ii;
@@ -80,6 +111,7 @@ void curveFit(double **arr, double res[], unsigned int resSize){
   for(i = 0; i < resSize; i++){
     for(j = 1; j < resSize; j++ )
       arr[i][j] = pow(arr[i][j], j);
+    //printArr(arr[i],resSize+1);
   }
   
   //solve lin eq
@@ -102,21 +134,12 @@ void curveFit(double **arr, double res[], unsigned int resSize){
 void setup() {
   Serial.begin(9600);
   Serial.print("Starting...\n");
-  //pixy.init();
-
+  pixy.init();
   createXYZArr();
 
+  fillTestValues();
 
-  // fill values
-  k = 0;
-  for (i = 0; i < Y_POLY_DEGREE + 1; i++){
-    for (j = 0; j < Y_POLY_DEGREE + 2; j++){
-      y[i][j] = ++k;
-      Serial.print(y[i][j]);
-      Serial.print(" ");
-    }
-    printArr(y[i],Y_POLY_DEGREE + 2);
-  }
+
 
 }
 
@@ -128,42 +151,82 @@ void loop() {
 
   // reset counters
   count = 0;
+  trueCount = 0;
   t0 = millis();
 
+  // For testing curve fit function
+  // curveFit(y,yPara, Y_POLY_DEGREE + 1);
+  // printArr(yPara,Y_POLY_DEGREE + 1);
+  // Serial.print(t0);
+  // Serial.print("\n");
+  // delay(10000);
+
+
+  do{
+    pixy.ccc.getBlocks();
+    for(i = 0; i < pixy.ccc.numBlocks; i++){
+      if(pixy.ccc.blocks[i].m_signature == PROJ_SIGNATURE){
+        t = ((double)millis() - (double)t0)/1000;
+        Serial.print("Time: ");
+        Serial.print(t);
+        Serial.print("\n");
+        if(trueCount < XNZ_POLY_DEGREE + 1 && count%SKIP_PARAM == 0){
+          timeArray(x[trueCount], XNZ_POLY_DEGREE + 2, t, pixy.ccc.blocks[i].m_x);
+          printArr(x[trueCount],XNZ_POLY_DEGREE + 2);
+        }
+        if(trueCount < Y_POLY_DEGREE + 1 && count%SKIP_PARAM == 0){
+          timeArray(y[trueCount], Y_POLY_DEGREE + 2, t, pixy.ccc.blocks[i].m_y);
+          printArr(y[trueCount],Y_POLY_DEGREE + 2);
+          trueCount++;
+        }
+        X[count] = pixy.ccc.blocks[i].m_x;
+        Y[count] = pixy.ccc.blocks[i].m_y;
+        T[count] = t;
+        count++;
+      }
+    }
+  } while(pixy.ccc.numBlocks/* && count <= Y_POLY_DEGREE + 2*/);
+
+
+  if(count > 0){
+    deltaT = millis() - t0;
+    Serial.print(count);
+    Serial.print(" readings in ");
+    Serial.print((double)deltaT / (double)1000);
+    Serial.print(" seconds\n");
+    Serial.print((double)count / (double)deltaT * (double)1000);
+    Serial.print(" readings per second\n");
+
+  Serial.print("xPara =");
+  curveFit(x,xPara, XNZ_POLY_DEGREE + 1);
+  printArr(xPara,XNZ_POLY_DEGREE + 1);
+  Serial.print("yPara =");
   curveFit(y,yPara, Y_POLY_DEGREE + 1);
-  // Serial.print(xPara[0]);
-  // Serial.print(" ");
-  // Serial.print(xPara[1]);
-  // Serial.print(" ");
   printArr(yPara,Y_POLY_DEGREE + 1);
+  Serial.print("X= ");
+  printArr(X,count);
+  Serial.print("Y= ");
+  printArr(Y,count);
+  Serial.print("T= ");
+  printArr(T,count);
+  Serial.print("tCount = ");
+  Serial.print(T[count - 1]);
+  Serial.print("\n\n\n");
+
+
+
+
+  Serial.print("Start: ");
   Serial.print(t0);
   Serial.print("\n");
-  delay(10000);
+  Serial.print("End: ");
+  Serial.print(millis());
+  Serial.print("\n");
 
+  }
 
-  // do{
-  //   pixy.ccc.getBlocks();
-  //   for(i = 0; i < pixy.ccc.numBlocks; i++){
-  //     if(pixy.ccc.blocks[i].m_signature == PROJ_SIGNATURE){
-  //       t = (t0 - millis()) / 1000;
+  // for(i = 0; i < Y_POLY_DEGREE + 1; i++)
+  //   printArr(y[i],Y_POLY_DEGREE + 2);
 
-  //       if(count < XNZ_POLY_DEGREE + 2)
-  //         timeArray(x[count], XNZ_POLY_DEGREE + 1, t, pixy.ccc.blocks[i].m_x);
-  //       timeArray(y[count], Y_POLY_DEGREE + 1, t, pixy.ccc.blocks[i].m_y);
-  //       count++;
-  //     }
-  //   }
-  // } while(pixy.ccc.numBlocks && count <= Y_POLY_DEGREE + 2);
-
-
-  // if(count > 0){
-  //   deltaT = millis() - t0;
-  //   Serial.print(count);
-  //   Serial.print(" readings in ");
-  //   Serial.print((double)deltaT / (double)1000);
-  //   Serial.print(" seconds\n");
-  //   Serial.print((double)count / (double)deltaT * (double)1000);
-  //   Serial.print(" readings per second\n");
-  // }
 
 }
